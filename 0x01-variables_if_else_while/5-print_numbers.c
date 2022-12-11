81a2003d80bd02f5466c150f65528918f3bf9256
w-pops <wambuiashley35@gmail.com>
2022-12-11 00:37:32 -0800

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,25 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/*
- * main - main block
- *
- * Description: prints all single digit number of base 10
- *
- * starting from 0, followed by new line
- *
+
+/**
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char c = '0';
+int num;
 
-while (c <= '9')
-{
-putchar(c);
-c++;
-}
-
+for (num = 0; num < 10; num++)
+printf("%d", num);
 putchar('\n');
 return (0);
 }
