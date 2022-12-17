5d716a9d3fce17b2d39c3a74eda1c844beca14f3
w-pops <wambuiashley35@gmail.com>
2022-12-17 11:09:19 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer, \
+ * starting from 00:00 to 23:59
+ * Return: nothing on success
+ */
+void jack_bauer(void)
+{
+int s = 0, e = 23, s2 = 0, e2 = 59;
+
+while (s <= e)
+{
+while (s2 <= e2)
+{
+_putchar(s < 9 ? 0 + '0' : s / 10 + '0');
+_putchar(s < 9 ? s + '0' : s % 10 + '0');
+_putchar(':');
+_putchar(s2 < 9 ? 0 + '0' : s2 / 10 + '0');
+_putchar(s2 < 9 ? s2 + '0' : s2 / 10 + '0');
+_putchar('\n');
+
+s2++;
+}
+s2 = 0;
+s++;
+}
+}
