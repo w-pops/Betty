3ceaa721e8ad9b36faa6fcd837a67b27ffddfc64
w-pops <wambuiashley35@gmail.com>
2022-12-18 10:36:16 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -1,7 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-
 /**
  *main - check the code
  *Return: Always 0.
@@ -9,26 +7,15 @@
 
 int main(void)
 {
-
 print_number(98);
-
 putchar('\n');
-
 print_number(402);
-
 putchar('\n');
-
 print_number(1024);
-
 putchar('\n');
-
 print_number(0);
-
 putchar('\n');
-
 print_number(-98);
-
 putchar('\n');
-
 return (0);
 }
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,23 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * print_number - print
- * @n: integer
- * Return: Always
+ * print_number - prints an integer
+ * @n: The integer to prints
+ * Return: Nothing!
  */
 void print_number(int n)
 {
+unsigned int k = n;
 
-unsigned int num = n;
 if (n < 0)
 {
+n *= -1;
+k = n;
 putchar('-');
-num = -num;
 }
-if ((num / 10) > 0)
-print_number(num / 10);
-
-putchar((num % 10) + '0');
+k /= 10;
+if (k != 0)
+print_number(k);
+putchar((unsigned int) n % 10 + '0');
 }
