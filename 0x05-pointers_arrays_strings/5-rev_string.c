3fad0de041839aef6c8b15081240ee74d92083f7
w-pops <wambuiashley35@gmail.com>
2022-12-19 11:38:29 -0800

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,22 +6,22 @@
  */
 void rev_string(char *s)
 {
+int i, j, k, temp;
 
-char tmp;
-int i, length1, length2;
-
-length1 = 0;
-length2 = 0;
-
-while (s[length1] != '\0')
-length1++;
-
-length2 = length1 - 1;
-for (i = 0; i <= length1; i++)
+i = 0;
+while (s[i] != '\0')
 {
-tmp = s[i];
-s[i] = s[length2];
-s[length2] = tmp;
-length2 -= 1;
+i++;
+}
+k = 0;
+j = i - 1;
+
+while (k < j)
+{
+temp = s[k];
+s[k] = s[j];
+s[j] =temp;
+k++;
+j--;
 }
 }
